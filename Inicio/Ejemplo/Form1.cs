fb6ddffb5ec7360079535f2330689e3a859de32f
hernan yarbi <hernan.yarbi@gmail.com>
2018-04-10 02:56:40 -0300

    segunda ventana

--- Inicio/Ejemplo/Form1.cs
+++ Inicio/Ejemplo/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Ventana;
 
 namespace Ejemplo
 {
@@ -23,7 +24,8 @@ namespace Ejemplo
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.Close();
+            NewWindow fVentana = new NewWindow();
+            fVentana.Show();
         }
     }
 }
